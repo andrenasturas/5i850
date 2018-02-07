082f1e0a415c5bdd214b6bc8d9061091cb2a2ff4
Benjamin Loglisci <loglisci@hotmail.fr>
2018-02-07 23:35:36 +0100

    Modif algoDEN

--- algoDEN.py
+++ algoDEN.py
@@ -1,18 +1,54 @@
 import torch
 from torch.autograd import Variable
-import random as rd
+from torch.nn.parameter import Parameter
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 class DEN(object):
 
-    def __init__(self, paramList, tau, mu, gamma, k, sigma)
-        self.paramList = paramList
+    def __init__(self, model, tau, mu, gamma, k, sigma, optim = optim.SGD, lr=1e-3, loss = nn.MSELoss()) :
+
+        '''
+        paramList de la forme nbCouche*(weightTensor,biasTensor)
+        '''
+        self.model = model
         self.tau = tau
         self.mu = mu
         self.gamma = gamma
         self.k = k
         self.sigma = sigma
+        self.init = False
+        self.optim = optim
+        self.loss = loss
+        self.lr = lr
     
-    def train(self):
+    def trainNext(self, x, y):
+        if not self.init :
+            trainL1(self, x, y)
+            self.init = True
+        else :
+            l = selectiveRetraining(self, x, y)
+            if l > self.tau:
+                dynamicExpansion(self, x, y)
+            split(self, x, y)
+
+
+    def forward(self, x):
+        self.model.forward()
+
+    def trainL1(self, x, y, n = 100):
+        op = self.optim(self.model.params(), self.lr)
+        l1_crit = nn.L1Loss(size_average=False)
+
+        for i in range(n):
+
+            loss = self.loss(self(x), y)
+
+            s = 0
+            for p in self.model.params() :
+                s += l1_crit(p, torch.zeros_like(p))
+            loss += self.mu * s
+            loss.backward()
+            op.step()
+        
